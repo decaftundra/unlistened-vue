9afcca5dab35ec8437d6c290083ac15c141d2bd4
julienbeghain <julienbeghain@gmail.com>
2021-11-13 18:10:08 +0100

    vue complete + ajout nom playlist

--- web/src/views/Unlistened.vue
+++ web/src/views/Unlistened.vue
@@ -5,11 +5,11 @@ prends un parametre :spotifyId pour récupérer ID playlist
 <template>
     <!-- Liste les playlists d'un utilisateur donné
 Lien cliquable vers unlistened -->
-  <h1>Unlistened</h1>
+  <h1>{{playlist.name}}</h1>
   <div>
     <hr>
-    <div class="list-unstyled" v-for="track in tracks" :key="track.spotifyId">
-      <h4 class="mt-0 mb-1">{{track.artistName + '-' + track.trackName}}</h4>
+    <div class="list-unstyled" v-for="track in playlist.tracks" :key="track.spotifyId">
+      <h4 class="mt-0 mb-1">{{track.artistName + ' - ' + track.trackName + ' (' + track.listenCount + ')'}}</h4>
     </div>
   </div>
 </template>
@@ -22,14 +22,15 @@ export default {
   components: {},
   data: () => ({
     error: '',
-    tracks: [],
+    playlist: {},
   }),
-  mounted: async function() {
+  mounted:  function() {
     
     fetch('http://localhost:4000/getDiscovery/'+ this.$route.params.playlistId)
       .then(res => res.json())
       .then(res => {
-        this.tracks = res;
+         console.log(res);
+        this.playlist = res;
       })
     
     
